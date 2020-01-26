40cad0af210e1f2377bd9ea41f081b1606b65229
ysandeepguptha3 <ysandeepguptha3@gmail.com>
2020-01-26 14:34:07 +0530

    adding a floder

---
+++ sandeep1/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title:Text("flutterGuptha"),
+        ),
+        body: Center(
+        child: Text("Hello World"),
+        )
+      )
+    );
+  }
+}
