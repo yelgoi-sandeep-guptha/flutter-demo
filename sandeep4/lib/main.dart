97256e8f89d2236a45d12c0db5e98c609bdcce8b
ysandeepguptha3 <ysandeepguptha3@gmail.com>
2020-01-27 18:33:22 +0530

    Stateful Hello Sandeep

---
+++ sandeep4/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Sandeep4",
+      home: new HomePage(),
+    );
+  }
+}
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Flutter Home Page"),
+      ),
+      body: new Center(
+        child :new Text("Hello Sandeep")
+      )
+    );
+  }
+}
+
+
