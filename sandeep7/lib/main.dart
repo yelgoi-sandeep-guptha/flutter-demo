3d669e561dc6c82860fb80fc598c6aa51bcd6043
ysandeepguptha3 <ysandeepguptha3@gmail.com>
2020-01-28 21:48:45 +0530

    Stateful Text alignments colors

---
+++ sandeep7/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: new Homepage(),
+    );
+  }
+}
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+
+
+
+class _HomepageState extends State<Homepage> {
+  String mytext="Hello FlutterGuptha";
+
+  void _changeText(){
+    setState(() {
+      if(mytext.startsWith("H")) {
+        mytext="SandeepFlutter";
+      }else{
+        mytext="Hello FlutterGuptha";
+      }
+    });
+  }
+
+
+
+
+  Widget _bodyWidget(){
+    return new Container(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(mytext),
+            new RaisedButton(
+              color: Colors.red,
+                child: new Text("Click",style: new TextStyle(
+                  color: Colors.white,
+                  fontSize: 30
+                ),),
+                onPressed: _changeText)
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: new AppBar(
+          title: new Text("Stateful MainAxisAlignment "),
+        ),
+        body: _bodyWidget()
+    );
+  }
+}
+
