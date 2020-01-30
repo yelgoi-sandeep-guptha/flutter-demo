e93301435b825d718ed7cf33e4d234469a1d8167
ysandeepguptha3 <ysandeepguptha3@gmail.com>
2020-01-30 23:19:12 +0530

    icon

---
+++ flutter_icon/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: new Container(
+        height: 200.0,
+        width: 200.0,
+        child: new Icon(Icons.flag),
+        foregroundDecoration: new ,
+      )
+    );
+
+  }
+}
