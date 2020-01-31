82d6e48ada5145c6a797ea8c697dbfb3a14d11f2
ysandeepguptha3 <ysandeepguptha3@gmail.com>
2020-01-31 21:57:30 +0530

    column-color-horizontal

---
+++ flutter_column/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main()=> runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                height:20,
+                width: 20,
+                color: Colors.red,
+              ),
+              Container(
+                height: 20,
+                width: 20,
+                color: Colors.green,
+              ),
+              Container(
+                height: 20,
+                width: 20,
+                color:Colors.yellow,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
